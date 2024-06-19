e8f2c2f8b7c91124e7257c1e0043d4baac633ded
spancavil <soancavil@gmail.com>
2024-06-19 11:31:41 -0300

    Added template Clase 12

--- Clase 12 - Autenticacion de usuarios I/example-jwt/middlewares/authenticate.js
+++ Clase 12 - Autenticacion de usuarios I/example-jwt/middlewares/authenticate.js
@@ -1,17 +1,5 @@
-import jwt from 'jsonwebtoken'
-import { JWT_SECRET } from '../config/config.js'
-
 export const authenticateToken = (req, res, next) => {
     try {
-        const authHeader = req.headers['authorization']
-        const token = authHeader && authHeader.split(' ')[1]
-        if (!token)return res.sendStatus(401)
-    
-        const payload = jwt.verify(token, JWT_SECRET)
-        req.user = payload
-        next()
     } catch (error) {
-        console.log(error);
-        res.sendStatus(403)
     }
 }
