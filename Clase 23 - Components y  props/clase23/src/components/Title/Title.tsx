018669a8cffd66868c8e635be3ea70fc5af4fc34
spancavil <soancavil@gmail.com>
2024-07-10 18:27:12 -0300

    Desarrollo clase 23

--- Clase 23 - Components y  props/clase23/src/components/Title/Title.tsx
+++ Clase 23 - Components y  props/clase23/src/components/Title/Title.tsx
@@ -5,6 +5,9 @@ type TitleProps = {
     title: string
 }
 
+// props = {title}
+// {title} = props
+
 const Title = ({title} : TitleProps) => {
   return (
     <h1 style={{fontSize: 45}} className='title'>{title}</h1>
