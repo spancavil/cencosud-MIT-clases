f3b57b2bfea93cbc3474a61e82ba0c45c8a239e4
spancavil <soancavil@gmail.com>
2024-07-17 18:42:00 -0300

    Desarrollo clase 29

--- Clase 29 - Redux and Router/clase29/src/components/Counter/Counter.tsx
+++ Clase 29 - Redux and Router/clase29/src/components/Counter/Counter.tsx
@@ -1,36 +1,61 @@
 import React, { useState } from "react"
 import styles from "./counter-styles.module.scss"
+import { useAppDispatch, useAppSelector } from "../../hooks/custom-redux"
+import { useDispatch } from "react-redux"
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "../../redux/features/slices/counterSlice"
 
 // type Props = {}
 
 const Counter = () => {
-
   const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState("")
+  const { value } = useAppSelector((state) => state.counter)
+  const dispatch = useAppDispatch()
+
+  console.log({ value })
 
   //const response = useState(0)
   //response será un array con 2 posiciones => 1 valor del state, 2 es la funcion que cambia ese state
 
   const handleMinus = (e: React.MouseEvent<HTMLButtonElement>) => {
     //Will add to quantity
-    console.log(e);
+    console.log(e)
     // count = count - 1
-    setCount(previousCount => previousCount - 1)
+    setCount((previousCount) => previousCount - 1)
+    dispatch(decrement())
   }
 
   const handlePlus = (e: React.MouseEvent<HTMLButtonElement>) => {
     //Will add to quantity
-    console.log(e);
+    console.log(e)
     // count = count - 1
-    setCount(previousCount => previousCount + 1)
+    setCount((previousCount) => previousCount + 1)
+    dispatch(increment())
   }
 
-  console.log(count);
-  
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value)
+  }
+
+  console.log(count)
+
   return (
     <div className={styles.main}>
-      <button onClick={handleMinus} className={styles.button}>-</button>
-      <span className={styles.text}>{count}</span>
-      <button onClick={handlePlus} className={styles.button}>+</button>
+      <button onClick={handleMinus} className={styles.button}>
+        -
+      </button>
+      <span className={styles.text}>{value}</span>
+      <button onClick={handlePlus} className={styles.button}>
+        +
+      </button>
+      <input value={inputValue} onChange={handleInputChange} />
+      <button onClick={() => dispatch(incrementByAmount(Number(inputValue)))}>
+        Confirm
+      </button>
     </div>
   )
 }
--- Clase 29 - Redux and Router/clase29/src/navigation/router.tsx
+++ Clase 29 - Redux and Router/clase29/src/navigation/router.tsx
@@ -8,6 +8,7 @@ import Home from "../pages/Home/Home"
 import News from "../pages/News/News"
 import NewsDetail from "../pages/NewsDetail/NewsDetail"
 import Counter from "../components/Counter/Counter"
+import NotFound from "../pages/NotFound/NotFound"
 
 const router: any = createBrowserRouter(
   createRoutesFromElements(
@@ -17,6 +18,7 @@ const router: any = createBrowserRouter(
       <Route path="/news/:id" element={<NewsDetail />} />
       {/* Declare more router and an error page */}
       <Route path="/counter" element = {<Counter/>} />
+      <Route path="*" element = {<NotFound/>}/>
     </Route>
   )
 )
